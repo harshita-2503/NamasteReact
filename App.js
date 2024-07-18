4be790069532b558dab5a5afd2bb86fb323f13b3
harshita-2503 <112571316+harshita-2503@users.noreply.github.com>
2024-07-18 20:21:49 +0530

    Add files via upload
    Episode 2

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// 1
+                         // Episode 1
 
 
 
@@ -81,6 +81,22 @@
 
 
 
+// const parent=React.createElement("div",{id: "parent"},[React.createElement("div",{id: "child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),React.createElement("div",{id: "child2"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])]);
+
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
+// console.log(parent);
+
+
+                    // Episode 2
+
+ import React from "react";
+ import ReactDOM from "react-dom/client"
+
+                    
 const parent=React.createElement("div",{id: "parent"},[React.createElement("div",{id: "child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),React.createElement("div",{id: "child2"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])]);
 
 
@@ -89,3 +105,5 @@ const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
 console.log(parent);
+
+                 
