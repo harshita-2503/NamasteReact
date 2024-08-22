674250780054da04564a08a340b9d25a5b7664a6
Harshita Mahajan <harshitamahajan2020@gmail.com>
2024-08-22 07:45:50 +0530

    Episode 12 part 8

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -18,17 +18,27 @@ const cartSlice=createSlice({
             // return newState
 
             //mutating the state here
+
+            //Redux Toolkit uses immer bts
             state.items.push(action.payload);
+
 
         },
 
         removeItem:(state,action)=>{
             state.items.pop();
         },
-        clearCart: (state)=>{
+
+        // originalState ={ items : ["pizza"]}
+        clearCart: (state,action)=>{
 
             //why not state=0 used
-            state.items.length=0;
+            // state.items.length=0;
+
+            // RTK -either mutate the original state or return a new state
+
+            return { items:[] }; //this new object will be replaced inside originalState ={ items :[] }
+
         },
 
     },
